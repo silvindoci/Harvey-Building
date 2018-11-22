9fc1bfed971fd6aafe04086d3cd4bb92af1df04c
silvindoci <silvesterndoci@hotmailcom>
2018-11-22 23:44:26 +0100

    s

--- js/main.js
+++ js/main.js
@@ -1,34 +1,37 @@
 //menu
 $(".toggle ").click(function() {
     $(this).toggleClass('on');
-    $(".toggle-menu ").toggleClass("active");
+    $(".toggle-menu ").toggleClass("active-toggle-menu");
 });
 
 
 
 
-//header bar menu
+
+
+
+//header bar menu 
 $('.headerbar').on('click', '.search-toggle', function(e) {
     var selector = $(this).data('selector');
 
     $(selector).toggleClass('show').find('.search-input').focus();
-    $(this).toggleClass('active');
+    $(this).toggleClass('active-search-toggle');
 
     e.preventDefault();
 });
 
+(function($) { //header search toggle on responsive device
 
-
-//header search toggle
-(function($) {
     $('#toggle-search').click(function() {
         $('#search-form, #toggle-search').toggleClass('open');
         return false;
     });
+
     $('#search-form input[type=submit]').click(function() {
         $('#search-form, #toggle-search').toggleClass('open');
         return true;
     });
+
     $(document).click(function(event) {
         var target = $(event.target);
 
@@ -37,17 +40,24 @@ $('.headerbar').on('click', '.search-toggle', function(e) {
         }
     });
 
-});
+})(jQuery);
+
+
+
+
 
 
 //red click tabs
 $('.restab .tap-p').click(function(event) {
-    $(this).addClass('active');
-    $(this).siblings().removeClass('active');
+    $(this).addClass('active1');
+    $(this).siblings().removeClass('active1');
 });
 
-//Flex slider
 
+
+
+
+//Flex slider
 $(function() {
     SyntaxHighlighter.all();
 });
