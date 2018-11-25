6ef409c09b5827e4f41669c007792004c5824f88
silvindoci <silvesterndoci@hotmailcom>
2018-11-25 09:11:24 +0100

    san

--- js/main.js
+++ js/main.js
@@ -48,25 +48,43 @@ $('.headerbar').on('click', '.search-toggle', function(e) {
 
 
 //red click tabs
-$('.restab .tap-p').click(function(event) {
-    $(this).addClass('active1');
-    $(this).siblings().removeClass('active1');
+$(".tab_content").hide();
+$(".tab_content:first").show();
+
+
+$("ul.tabs li").click(function() {
+    $(".tab_content").hide();
+    var activeTab = $(this).attr("rel");
+    $("#" + activeTab).fadeIn();
+
+    $("ul.tabs li").removeClass("active");
+    $(this).addClass("active");
+
+    $(".tab_drawer_heading").removeClass("d_active");
+    $(".tab_drawer_heading[rel^='" + activeTab + "']").addClass("d_active");
+
+});
+$(".tab_container").css("min-height", function() {
+    return $(".tabs").outerHeight() + 50;
 });
 
+$(".tab_drawer_heading").click(function() {
 
+    $(".tab_content").hide();
+    var d_activeTab = $(this).attr("rel");
+    $("#" + d_activeTab).fadeIn();
+
+    $(".tab_drawer_heading").removeClass("d_active");
+    $(this).addClass("d_active");
+
+    $("ul.tabs li").removeClass("active");
+    $("ul.tabs li[rel^='" + d_activeTab + "']").addClass("active");
+});
 
 
 
 //Flex slider
-$(function() {
-    SyntaxHighlighter.all();
-});
-$(window).load(function() {
-    $('.flexslider').flexslider({
-        animation: "slide",
-        controlNav: "thumbnails",
-        start: function(slider) {
-            $('body').removeClass('loading');
-        }
-    });
+$('.flexslider').flexslider({
+    animation: "slide",
+    controlNav: "thumbnails",
 });
